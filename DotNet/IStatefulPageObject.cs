50864183f2ee605e173d7f11c96e060a3cdaf0d6
unknown <vanproyaa@OST-5CG5442BJK.ostusa.com>
2016-02-22 18:09:47 -0500

    Adding a page state interface

---
+++ DotNet/IStatefulPageObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriverExtended
+{
+    /// <summary>
+    /// Adds statful functionality to a page object
+    /// </summary>
+    public interface IStatefulPageObject
+    {
+        /// <summary>
+        /// Adds a state to the page object
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        void AddState(string key, string value);
+
+        /// <summary>
+        /// retives a page state
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        PageState GetState(string key);
+
+        /// <summary>
+        /// updates a state to a page
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        void UpdateState(string key, string value);
+
+        /// <summary>
+        /// remove a state from a page
+        /// </summary>
+        /// <param name="key"></param>
+        void RemoveState(string key);
+    }
+}
---
+++ DotNet/PageState.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriverExtended
+{
+    /// <summary>
+    /// An object used to trake state with in a page object
+    /// </summary>
+    public class PageState
+    {
+        private string stateKey;
+        private string stateValue;
+        private DateTime lastInteraction;
+
+        /// <summary>
+        /// The key used to interact with a page state
+        /// </summary>
+        public string Key { get { return stateKey; } }
+
+        /// <summary>
+        /// The value of a page state
+        /// </summary>
+        public string Value
+        {
+            get { return stateValue; }
+
+            set
+            {
+                stateValue = value;
+                lastInteraction = DateTime.Now;
+            }
+        }
+
+        /// <summary>
+        /// the last time a state has been updated
+        /// </summary>
+        public DateTime LastInteraction
+        {
+            get
+            {
+                return lastInteraction;
+            }
+        }
+        
+        /// <summary>
+        /// constuctor to track state
+        /// </summary>
+        /// <param name="key">used to interact with a state</param>
+        /// <param name="value">the current value of the state</param>
+        public PageState(string key, string value)
+        {
+            stateKey = key;
+            Value = value;
+        }
+    }
+}
