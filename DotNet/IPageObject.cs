f6375999847b510e3d0465d4f22f44b13740fccc
unknown <vanproyaa@OST-5CG5442BJK.ostusa.com>
2016-02-22 17:45:59 -0500

    page object interfaces

---
+++ DotNet/IPageObject.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriverExtended
+{
+    /// <summary>
+    /// A basic page object interface
+    /// </summary>
+    public interface IPageObject
+    {
+        /// <summary>
+        /// Any setup a page object may need
+        /// </summary>
+        void Setup();
+
+        /// <summary>
+        /// Navigate to an address
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="url"></param>
+        void Navigate(IWebDriver driver, string url);
+
+        /// <summary>
+        /// Navigate to an address
+        /// </summary>
+        /// <param name="url"></param>
+        void Navigate(string url);
+
+        /// <summary>
+        /// Navigate to an address
+        /// </summary>
+        /// <param name="driver"></param>
+        void Navigate(IWebDriver driver);
+
+        /// <summary>
+        /// Do any clenup the page object needs
+        /// </summary>
+        void TearDown();
+    }
+}
